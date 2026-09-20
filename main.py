1acfde453de6fc41cdc32c2ed7635de1e60070ce
omarbshah <omarshah990@gmail.com>
2026-09-20 06:01:33 +0530

    Add GET /users route using get_db dependency

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from models import User
 
 app = FastAPI()
 
@@ -6,3 +10,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "DevPulse is alive"}
+
+
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
