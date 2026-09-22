fa9c341d9b173f741b6f7abfe21cfad075af25e4
ZehaanArshad <zehaanarshad@gmail.com>
2026-09-22 10:53:14 +0530

    Add ProtectedRoute component and authentication utility functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,19 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import LinkPlatform from './pages/LinkPlatform'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/link-platform" element={<LinkPlatform />} />
---
+++ src/api/auth.js
@@ -0,0 +1,17 @@
+let token = null
+
+export function setToken(newToken) {
+  token = newToken
+}
+
+export function getToken() {
+  return token
+}
+
+export function clearToken() {
+  token = null
+}
+
+export function isLoggedIn() {
+  return token !== null
+}
